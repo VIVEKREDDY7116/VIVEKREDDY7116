a84c5a250a74e6a790d56fa357a50e8c502356e5
VIVEKREDDY7116 <84090172+VIVEKREDDY7116@users.noreply.github.com>
2021-07-21 21:01:17 +0530

    Add files via upload

---
+++ CCPOT6.C
@@ -0,0 +1,13 @@
+/* WAP TO FIND AREANOF THE CIRCLE */
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+float ac,r;
+clrscr();
+printf(" enter radius of the circle \n");
+scanf("%f",&r);
+ac=3.14*r*r;
+printf("area of the circle =%f",ac);
+return 0;
+}
